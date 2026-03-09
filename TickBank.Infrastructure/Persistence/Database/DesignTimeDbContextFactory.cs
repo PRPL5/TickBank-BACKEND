941e0cc93fa4dd32f32b4d253bbc40800a948c3f
PRPL5 <86297717+PRPL5@users.noreply.github.com>
2026-03-09 16:53:13 +0100

    connection to supabase

--- TickBank.Infrastructure/Persistence/Database/DesignTimeDbContextFactory.cs
+++ TickBank.Infrastructure/Persistence/Database/DesignTimeDbContextFactory.cs
@@ -1,16 +1,24 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace TickBank.Infrastructure.Persistence.Database
 {
-    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
-    {
-        public ApplicationDbContext CreateDbContext(string[] args)
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-            // Adjust connection string for your dev machine if needed
-            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TickBankDB;Trusted_Connection=True;");
-            return new ApplicationDbContext(optionsBuilder.Options);
-        }
-    }
+    //public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    //{
+    //    public ApplicationDbContext CreateDbContext(string[] args)
+    //    {
+    //        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            
+    //        var configuration = new ConfigurationBuilder()
+    //            .SetBasePath(Path.GetFullPath("../TickBank.Web"))
+    //            .AddJsonFile("appsettings.json")
+    //            .Build();
+            
+    //        var connectionString = configuration.GetConnectionString("DefaultConnection");
+    //        optionsBuilder.UseNpgsql(connectionString);
+    //        return new ApplicationDbContext(optionsBuilder.Options);
+    //    }
+    //}
 }
--- TickBank.Web/Program.cs
+++ TickBank.Web/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")) );
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")) );
 
 // Register MediatR handlers from the Application assembly
 builder.Services.AddMediatR(cfg =>
